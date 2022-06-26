f985b74dd4a8f61cd221bae7e69a6a32c6b010fe
meitarbasson <meitarbass@gmail.com>
2022-06-26 08:52:13 +0300

    2 first sections of day 17, done.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20 ,25, 0]
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD")).keyboardType(.decimalPad)
+                }
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2..<100) {
+                        Text("\($0) people")
+                    }
+                }
+            }.navigationTitle("WeSplit")
+        }
     }
 }
 
