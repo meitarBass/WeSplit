d26cd392dac52598398651f5d866785bce0b92f9
meitarbasson <meitarbass@gmail.com>
2022-06-27 13:38:47 +0300

    Finished WeSplit version, including challenges of day 18.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,25 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @FocusState private var amountIsFocused: Bool
+    
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
     let tipPercentages = [10, 15, 20 ,25, 0]
     
+    var currentCurrency: FloatingPointFormatStyle<Double>.Currency {
+        .currency(code: Locale.current.currencyCode ?? "USD")
+    }
+    
+    var totalPerPerson: Double {
+        // Caclculate the total per person here
+        let peopleCount = Double(numberOfPeople)
+        let grandTotal = self.total
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var total: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
+    }
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD")).keyboardType(.decimalPad)
+                    TextField("Amount", value: $checkAmount, format: currentCurrency).keyboardType(.decimalPad).focused($amountIsFocused)
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                } header: {
+                    Text("How much tip do you want to leave?")
                 }
                 
                 Picker("Number of people", selection: $numberOfPeople) {
                     ForEach(2..<100) {
-                        Text("\($0) people")
+                        Text("\($0) people").tag($0)
                     }
                 }
+                
+                Section {
+                    Text(totalPerPerson, format: currentCurrency)
+                }header: {
+                    Text("Total per person:")
+                }
+                
+                Section {
+                    Text(total, format: currentCurrency)
+                }header: {
+                    Text("The total bill (tip included):")
+                }
+                
             }.navigationTitle("WeSplit")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
         }
     }
 }
